2ba5979c93413d43550f3d02cdc6d6a7430efe16
Ari Gibson <arimgibson@gmail.com>
2022-02-19 18:12:32 -0700

    Stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import apiRoutes from "./api/index.js"
-// import * as render from "./views/renders.js";
+import cors from "cors";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -9,16 +9,17 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/'));
 app.use("/api", apiRoutes);
 
+
 // Sanity check
 app.get("/ping", (req, res) => {res.send("Pong!")})
 
 app.get("/", (req, res) => {
-	// render.home(res);
 	res.sendFile(__dirname + "/public/")
 })
 
